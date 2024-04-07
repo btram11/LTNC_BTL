ceecc64deb74d6a66fd2f03978c8a954d62344a9
btram11 <misa1101@gmail.com>
2024-04-07 10:59:00 +0700

    Fixing UI and add it to mainview.csproj

--- MainView/Dashboard.xaml.cs
+++ MainView/Dashboard.xaml.cs
@@ -17,7 +17,7 @@ namespace MainView
     /// <summary>
     /// Interaction logic for Dashboard.xaml
     /// </summary>
-    public partial class Dashboard : Window
+    public partial class Dashboard : UserControl
     {
         public Dashboard()
         {
