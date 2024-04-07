83695a5a2e60b8db0eb448a8711580ad2a5663e6
btram11 <misa1101@gmail.com>
2024-04-07 19:47:40 +0700

    Rearrange views and add some viewmodels

---
+++ ViewModels/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    public class DashboardViewModel: ViewModelBase
+    {
+        public DashboardViewModel()
+        {
+            
+        }
+    }
+}
